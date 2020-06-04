8f5e9099abd9c307fe2677b7b2632a6d621335a4
Fabrizio Montesi <famontesi@gmail.com>
2020-06-04 09:39:12 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -it --rm --name jolie-build -v "$(pwd)/../jolie":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn -T 1C clean install
-docker run -it --rm --name jolie-installer-build -v "$(pwd)/jolie_installer":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn -T 1C clean install
+docker run -it --rm --name jolie-build --user $(id -u):$(id -g) -v "$(pwd)/../jolie":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn -T 1C clean install
+docker run -it --rm --name jolie-installer-build --user $(id -u):$(id -g) -v "$(pwd)/jolie_installer":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn -T 1C clean install
